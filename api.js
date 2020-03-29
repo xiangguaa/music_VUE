d0647e5fe82240629008711b62c0a5b6a8fcaa79
shuo.zheng <428384851@qq.com>
2020-03-29 23:05:12 +0800

    简单的Vue项目

---
+++ api.js
@@ -0,0 +1,43 @@
+
+
+var api = {
+    serverDomain:"https://autumnfish.cn/",
+    clog:function(){
+        console.log("clog function!!!")
+        console.log(axios)
+    },
+
+
+
+    searchMusic:function(){
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+export {
+    api
+}
